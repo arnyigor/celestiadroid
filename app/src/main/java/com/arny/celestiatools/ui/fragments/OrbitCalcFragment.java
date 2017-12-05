10a0836dad7ac7ebedff603dc7a852d620c255ee
arny <arnyigor@gmail.com>
2017-12-05 23:43:42 +0300

    calc dialog

--- app/src/main/java/com/arny/celestiatools/ui/fragments/OrbitCalcFragment.java
+++ app/src/main/java/com/arny/celestiatools/ui/fragments/OrbitCalcFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 
 import android.widget.EditText;
 import android.widget.TextView;
+import com.arny.arnylib.utils.CalculatorDialog;
 import com.arny.arnylib.utils.DateTimeUtils;
 import com.arny.arnylib.utils.MathUtils;
 import com.arny.arnylib.utils.Utility;
@@ -24,149 +25,100 @@ import com.arny.celestiatools.utils.astro.OrbitCalcMotion;
  */
 public class OrbitCalcFragment extends Fragment {
 
-    private Context context;
-    private EditText edtMass;
-    private EditText edtRadius;
-    private EditText edtHeightPeri;
-    private EditText edtVelPeri;
-    private TextView tvResult;
-    private double mass;
-    private double radius;
-    private double Hp;
-    private double Vp;
+	private Context context;
+	private EditText edtMass;
+	private EditText edtRadius;
+	private EditText edtHeightPeri;
+	private EditText edtVelPeri;
+	private TextView tvResult;
+	private double mass;
+	private double radius;
+	private double Hp;
+	private double Vp;
 
-    public OrbitCalcFragment() {
-        // Required empty public constructor
-    }
+	public OrbitCalcFragment() {
+		// Required empty public constructor
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_orbit_calc, container, false);
-        context = container.getContext();
-        initUI(view);
-        initListeners();
-        return view;
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_orbit_calc, container, false);
+		context = container.getContext();
+		initUI(view);
+		initListeners();
+		return view;
+	}
 
-    private void initListeners() {
-        edtMass.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+	private void initListeners() {
+		edtMass.setOnClickListener(view -> new CalculatorDialog(context,edtMass.getText().toString(), result -> {
+			edtMass.setText(result);
+			calc();
+		}).show());
+		edtRadius.setOnClickListener(view -> new CalculatorDialog(context,edtRadius.getText().toString(), result -> {
+			edtRadius.setText(result);
+			calc();
+		}).show());
+		edtHeightPeri.setOnClickListener(view -> new CalculatorDialog(context,edtHeightPeri.getText().toString(), result -> {
+			edtHeightPeri.setText(result);
+			calc();
+		}).show());
+		edtVelPeri.setOnClickListener(view -> new CalculatorDialog(context,edtVelPeri.getText().toString(), result -> {
+			edtVelPeri.setText(result);
+			calc();
+		}).show());
+	}
 
-            }
+	private void calc() {
+		validateVariables();
+		StringBuilder results = new StringBuilder();
+		if (Vp <= 0) {
+			CircularMotion circularMotion = OrbitCalcMotion.calcCircularOrbit(mass, radius, Hp);
+			results.append("1я космическая:").append(MathUtils.round(circularMotion.getV1(), 6)).append("м/с\n");
+			results.append("2я космическая:").append(MathUtils.round(circularMotion.getV2(), 6)).append("м/с\n");
+			results.append("Ускорение:").append(MathUtils.round(circularMotion.getUskorenie(), 6)).append("м/с\n");
+			int hour = circularMotion.getHour();
+			int min = circularMotion.getMin();
+			int sec = circularMotion.getSec();
+			results.append("Период:").append(DateTimeUtils.convertTime(DateTimeUtils.convertTime(hour, min, sec)));
+		} else {
+			EllipseMotion ellipseMotion = OrbitCalcMotion.calcEllipseOrbit(mass, radius, Hp, Vp);
+			results.append("1я космическая:").append(MathUtils.round(ellipseMotion.getV1(), 6)).append("м/с\n");
+			results.append("2я космическая:").append(MathUtils.round(ellipseMotion.getV2(), 6)).append("м/с\n");
+			results.append("Скорость апогея:").append(MathUtils.round(ellipseMotion.getVa(), 6)).append("м/с\n");
+			results.append("Эксцентриситет:").append(ellipseMotion.getEcc()).append("\n");
+			results.append("Апогей:").append(MathUtils.round(ellipseMotion.getHa(), 6)).append("\n");
+			results.append("Ускорение:").append(MathUtils.round(ellipseMotion.getUskorenie(), 6)).append("м/с2\n");
+			int hour = ellipseMotion.getHour();
+			int min = ellipseMotion.getMin();
+			int sec = ellipseMotion.getSec();
+			results.append("Период:").append(DateTimeUtils.convertTime(DateTimeUtils.convertTime(hour, min, sec)));
+		}
+		tvResult.setText(results.toString());
+	}
 
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+	private double validateInputDouble(EditText editText){
+		String s = editText.getText().toString();
+		if (Utility.empty(s)) {
+			return 0.0;
+		}
+		s = s.trim();
+		s = Utility.match(s, "-?\\d+(?:\\.\\d*)?(?:[eE][+\\-]?\\d+)?", 0);
+		return Double.parseDouble(s);
+	}
 
-            }
+	private void validateVariables() {
+		mass = validateInputDouble(edtMass);
+		radius = validateInputDouble(edtRadius);
+		Hp = validateInputDouble(edtHeightPeri);
+		Vp = validateInputDouble(edtVelPeri);
+	}
 
-            @Override
-            public void afterTextChanged(Editable editable) {
-                calc();
-            }
-        });
-        edtRadius.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                calc();
-            }
-        });
-        edtHeightPeri.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                calc();
-            }
-        });
-        edtVelPeri.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                calc();
-            }
-        });
-    }
-
-    private void calc(){
-        validateVariables();
-        StringBuilder results = new StringBuilder();
-        if (Vp <= 0) {
-            CircularMotion circularMotion = OrbitCalcMotion.calcCircularOrbit(mass, radius, Hp);
-            results.append("1я космическая:").append(MathUtils.round(circularMotion.getV1(),6)).append("м/с\n");
-            results.append("2я космическая:").append(MathUtils.round(circularMotion.getV2(),6)).append("м/с\n");
-            results.append("Ускорение:").append(MathUtils.round(circularMotion.getUskorenie(),6)).append("м/с\n");
-            int hour = circularMotion.getHour();
-            int min = circularMotion.getMin();
-            int sec = circularMotion.getSec();
-            results.append("Период:").append(DateTimeUtils.convertTime(DateTimeUtils.convertTime(hour,min,sec)));
-        }else{
-            EllipseMotion ellipseMotion = OrbitCalcMotion.calcEllipseOrbit(mass, radius, Hp, Vp);
-            results.append("1я космическая:").append(MathUtils.round(ellipseMotion.getV1(),6)).append("м/с\n");
-            results.append("2я космическая:").append(MathUtils.round(ellipseMotion.getV2(),6)).append("м/с\n");
-            results.append("Скорость апогея:").append(MathUtils.round(ellipseMotion.getVa(),6)).append("м/с\n");
-            results.append("Эксцентриситет:").append(ellipseMotion.getEcc()).append("\n");
-            results.append("Апогей:").append(MathUtils.round(ellipseMotion.getHa(),6)).append("\n");
-            results.append("Ускорение:").append(MathUtils.round(ellipseMotion.getUskorenie(),6)).append("м/с2\n");
-            int hour = ellipseMotion.getHour();
-            int min = ellipseMotion.getMin();
-            int sec = ellipseMotion.getSec();
-            results.append("Период:").append(DateTimeUtils.convertTime(DateTimeUtils.convertTime(hour,min,sec)));
-        }
-        tvResult.setText(results.toString());
-    }
-
-    private void validateVariables() {
-        String massStr = edtMass.getText().toString();
-        String radStr = edtRadius.getText().toString();
-        String hPeriStr = edtHeightPeri.getText().toString();
-        String velPeriStr = edtVelPeri.getText().toString();
-        mass = !Utility.empty(massStr) ? Double.parseDouble(massStr) : 0;
-        radius = !Utility.empty(radStr) ? Double.parseDouble(radStr) : 0;
-        Hp = !Utility.empty(hPeriStr) ? Double.parseDouble(hPeriStr) : 0;
-        Vp = !Utility.empty(velPeriStr) ? Double.parseDouble(velPeriStr) : 0;
-    }
-
-    private void initUI(View view) {
-        edtMass = view.findViewById(R.id.edtMass);
-        edtRadius = view.findViewById(R.id.edtRadius);
-        edtHeightPeri = view.findViewById(R.id.edtHeightPeri);
-        edtVelPeri = view.findViewById(R.id.edtVelPeri);
-        tvResult = view.findViewById(R.id.tvResult);
-    }
-
-
-
-
-
-
-
+	private void initUI(View view) {
+		edtMass = view.findViewById(R.id.edtMass);
+		edtRadius = view.findViewById(R.id.edtRadius);
+		edtHeightPeri = view.findViewById(R.id.edtHeightPeri);
+		edtVelPeri = view.findViewById(R.id.edtVelPeri);
+		tvResult = view.findViewById(R.id.tvResult);
+	}
 
 }
